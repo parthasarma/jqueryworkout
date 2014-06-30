cd9af0429f19f161af9545da5b067bdb52edb4a1
Partha Sarma <ambition.logic@gmail.com>
2014-07-01 00:33:10 +0530

    updated to jumpforfun_v2.1

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,12 @@
 //adding behavior to the images
 $(document).ready(function () {
 	$(".image").click(function () {
-		$(".image p").remove();
+		//$(".image p").remove();
 		var discount = Math.floor((Math.random() * 5) + 5);
 		var discount_msg = "<p>You get a discount of $" + discount + "%</p>";
 		$(this).append(discount_msg);
+		$(".image").each(function () {
+			$(this).unbind("click");
+		});
 	});//end of the click
-});//end of doc ready
+});//end of doc ready 
