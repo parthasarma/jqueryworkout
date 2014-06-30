69901b6e2b063869c97fe266d7576724c57ca9e3
Partha Sarma <ambition.logic@gmail.com>
2014-06-30 21:13:31 +0530

    base template

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,3 @@
-// $(document).ready(function () {
-// 	$("#clickMe").click(function () {
-// 		$("img").fadeIn(1000);
-// 		$("#picframe").slideToggle("slow");
-// 	});
-// 	$("#move_up").click( function() {
-// 		$("#change_me").animate({top:30},200); 
-// 	});//end move_up
-// 	$("#move_down").click( function() {	
-// 		$("#change_me").animate({top:500},2000);  
-// 	});//end move_down
-// 	$("#color").click( function() {
-// 		$("#change_me").css("color", "purple");	
-// 	});//end color
-// 	$("#disappear").click( function() {
-// 		$("#change_me").toggle('slow'); 
-// 	});//end disappear
-// });
-
 //adding behavior to the images
 $(document).ready(function () {
 	$(".image").click(function () {
