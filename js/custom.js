c0561a1fed9e84a3fbd8aff38bdae77c42b35be3
Partha Sarma <ambition.logic@gmail.com>
2014-06-30 21:06:16 +0530

    Furry_friend_V1.0 saved

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,9 @@
-// $(document).ready(function () {
-// 	$("#clickMe").click(function () {
-// 		$("img").fadeIn(1000);
-// 		$("#picframe").slideToggle("slow");
-// 	});
+$(document).ready(function () {
+	$("#clickMe").click(function () {
+		$("img").fadeIn(1000);
+		$("#picframe").slideToggle("slow");
+	});
+});	
 // 	$("#move_up").click( function() {
 // 		$("#change_me").animate({top:30},200); 
 // 	});//end move_up
@@ -18,11 +19,11 @@
 // });
 
 //adding behavior to the images
-$(document).ready(function () {
-	$(".image").click(function () {
-		$(".image p").remove();
-		var discount = Math.floor((Math.random() * 5) + 5);
-		var discount_msg = "<p>You get a discount of $" + discount + "%</p>";
-		$(this).append(discount_msg);
-	});//end of the click
-});//end of doc ready
+// $(document).ready(function () {
+// 	$(".image").click(function () {
+// 		$(".image p").remove();
+// 		var discount = Math.floor((Math.random() * 5) + 5);
+// 		var discount_msg = "<p>You get a discount of $" + discount + "%</p>";
+// 		$(this).append(discount_msg);
+// 	});//end of the click
+// });//end of doc ready
