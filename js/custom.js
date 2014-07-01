a42fdd38d2af36c5325bc4826e2ab03560955dc5
Partha Sarma <ambition.logic@gmail.com>
2014-07-01 23:15:09 +0530

    added comments

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,21 @@
 //adding behavior to the images
 $(document).ready(function () {
+	//add the function to the images
 	$(".image").click(checkForCode);
-	var checkForCode = function () {
+
+	//function to display dicount message
+	function checkForCode () {
 		//$(".image p").remove();
+		//find a random number
 		var discount = Math.floor((Math.random() * 5) + 5);
+		//create the message string
 		var discount_msg = "<p>You get a discount of $" + discount + "%</p>";
+		//append the message string to the image
 		$(this).append(discount_msg);
+
+		//remove the event
 		$(".image").each(function () {
 			$(this).unbind("click");
 		});
-	});//end of the click
+	}//end of the click
 });//end of doc ready 
