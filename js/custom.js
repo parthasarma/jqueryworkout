f35979addc946a6c3e89ac7ab04e2968190265b9
Partha Sarma <ambition.logic@gmail.com>
2014-07-02 00:24:25 +0530

    major improvement

--- js/custom.js
+++ js/custom.js
@@ -3,13 +3,40 @@ $(document).ready(function () {
 	//add the function to the images
 	$(".image").click(checkForCode);
 
+	//find a random number
+	function generateRandom (number) {
+		var randomNumber = Math.floor((Math.random() * number));
+		return randomNumber;
+	}
+
+	//to hide discount within a random 
+	var hideCode = function () {
+		//to represent any of the four divs randomly
+		var randNum = generateRandom(4);
+
+		$(".image").each(function (index, value) {
+			if (randNum == index) {
+				$(this).append("<span id='has_discount'></span>");
+				return false;
+			}
+		});
+	}
+
+	hideCode();
+
 	//function to display dicount message
 	function checkForCode () {
 		//$(".image p").remove();
-		//find a random number
-		var discount = Math.floor((Math.random() * 5) + 5);
-		//create the message string
-		var discount_msg = "<p>You get a discount of $" + discount + "%</p>";
+		
+		var discount_msg;		
+		if($.contains(this, document.getElementById("has_discount"))){
+			//create the random discount
+			var discount = generateRandom(5) + 5;
+				//create the message string
+			discount_msg = "<p>You get a discount of $" + discount + "%</p>";
+		}else{
+			discount_msg = "<p>Sorry no discount this time</p>";
+		}
 		//append the message string to the image
 		$(this).append(discount_msg);
 
@@ -18,4 +45,5 @@ $(document).ready(function () {
 			$(this).unbind("click");
 		});
 	}//end of the click
+
 });//end of doc ready 
