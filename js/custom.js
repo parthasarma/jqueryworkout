c26cb39df3b2047a77b5470ad90db5020f40aa08
Partha Sarma <ambition.logic@gmail.com>
2014-07-06 01:31:48 +0530

    intial funtionality to both buttons

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-	// body...
-});
+
+	var v = false;
+
+	// setting property of each button
+	$("button#vegOn").click(function () {
+		if (v == false) {
+			v = true;
+		}
+	});//end of vegOn button
+
+	$("button#restoreMe").click(function () {
+		if (v == true) {
+			v = false;
+		}
+	});//end of restoreMe button
+});//end of document ready
