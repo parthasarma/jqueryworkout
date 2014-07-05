aae5c37dffe7dd0cf24f4987dbe7b4c8c8f2a9a8
Partha Sarma <ambition.logic@gmail.com>
2014-07-06 02:04:03 +0530

    detached element from DOm using DOM traversal method

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
 
 	var v = false;
-
+	var $f, $m;
 	// setting property of each button
 	$("button#vegOn").click(function () {
 		if (v == false) {
+			$f = $(".fish").parent().parent().detach();//relationship chaining
 			v = true;
 		}
+		
 	});//end of vegOn button
 
 	$("button#restoreMe").click(function () {
 		if (v == true) {
 			v = false;
 		}
+		location.reload();
 	});//end of restoreMe button
 });//end of document ready
