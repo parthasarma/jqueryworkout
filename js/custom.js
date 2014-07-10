d7dcaee95b2a9b01fae22f54692c71b43d2b94bc
Partha Sarma <ambition.logic@gmail.com>
2014-07-11 00:46:47 +0530

    merged the content of jQuery_animate_V1.0 branch

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
-	$(".rectangle").find(".square_small").animate({left: 280}, "slow");
+	$(".rectangle").find(".square_small").animate({left: $(".rectangle").width() - $(".rectangle").find(".square_small").width()}, "slow");
+
+	function animation () {
+		var option = {
+			duration : 800,
+			easing : "linear"
+		};
+
+	$(".square-big")
+		.find(".square_small")
+			.animate(
+			{
+				left: 280,
+				top: 280
+			},
+			options
+			)
+			.animate(
+			{
+				left: 0,
+			},
+			options
+			)			
+			.animate(
+			{
+				left: 200,
+				top: 0
+			},
+			options
+			)
+			.animate(
+			{
+				left: 0,
+			},
+			$.extend(true, options, {
+				complete: function () {
+					animation();
+				}
+			})
+			);
+	}
 });
